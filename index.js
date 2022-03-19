0922e1b452bb3570799285c58fce505a865e009b
Ahashanahmed2 <ahashanahmed@yahoo.com>
2022-03-19 17:08:58 +0800

    bb

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const AdmZip = require("adm-zip");
 const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
-
+const dotenv = require("dotenv")
+dotenv.config()
 const app = express();
 const http = require("http");
 const bodyParser = require("body-parser");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+global.kk = io;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -22,9 +24,21 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
+ 
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const mimetype = fileTypes.test(file.mimetype);
+    const extname = fileTypes.test(path.extname(file.originalname));
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb("Error: Only jpeg or jpg or png Acceptable");
+  },
+});
 
 app.get("/", (req, res) => {
   if (!fs.existsSync("./src")) {
@@ -54,17 +68,19 @@ app.post("/upload", upload.array("avatar",10), (req, res) => {
 
               Tesseract.recognize(dat, "ben", {
                 logger: (m) => {
-                  let kk = io.of("/upload");
-                  setInterval(() => {
-                    kk.emit('logg',m)
-                  },1000)
+                  
+              
+                 
+                  
+                    kk.emit("logg", m);
+                  
            
                
                 console.log(m)
                 },
               }).then(({ data: { text } }) => {
                 let fil = value.replace(".jpg", ".text");
-let kk = io.of("/upload");
+
                   setInterval(() => {
                     kk.emit('file',fil)
                   },1000)
@@ -129,14 +145,15 @@ app.delete("/delete", (req, res) => {
 app.post("/itemd", (req, res) => {
   let id = req.body.id;
 
-  fs.unlink(`./src/download/${id}`,err=>{
+  fs.unlink(`./src/download/${id}`, err => {
     if (err) {
       console.log(err)
     }
-      res.send(id);
+    res.send(id);
   })
 
-});
+
+    });
 
 app.post("/itemu", (req, res) => {
   let id = req.body.id;
@@ -152,18 +169,20 @@ app.get("/reload", (req, res) => {
   res.redirect('/upload')
 })
 
-let kk = io.of("/upload")
-kk.on("connection", (socket) => {
+
+kk.on("connection", e => {
  
-    fs.readdir("./src/uploads", (err, file) => {
-      if (err) return res.send(err);
-setTimeout(() => {
-   kk.emit("upload", file);
+  fs.readdir("./src/uploads", (err, file) => {
+    if (err) return res.send(err);
+
+
   
-}, 10);
+    kk.emit("upload", file);
+ 
+
      
      
-    });
+  });
   
 
 
@@ -171,17 +190,20 @@ setTimeout(() => {
     fs.readdir("./src/download", (err, data) => {
       if (err) return console.log(err)
       
-        setTimeout(() => {
-          kk.emit("download", data);
-        }, 10);
+      kk.emit("download", data);
+    
+       
+        
       
     })
   }
   if (fs.existsSync("./src/files.zip")) {
-   fs.existsSync("./src/files.zip")
-    setTimeout(() => {
-      kk.emit("delete", fs.existsSync("./src/files.zip"));
-    }, 1000);
+    fs.existsSync("./src/files.zip")
+ 
+    kk.emit("delete", fs.existsSync("./src/files.zip"));
+   
+      
+    
   }
-});
-app.listen(3000, () => console.log("server is connect"));
+})
+server.listen(3000, () => console.log("server is connect"));
